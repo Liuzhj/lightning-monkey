ce7cf67ac894b1e04651a02d020771db4c3495ce
Kevin Yang <yangjie1@gridsum.com>
2019-02-26 20:21:56 +0800

    commit prototype files.

---
+++ pkg/certs/k8s_certs_generator.go
@@ -0,0 +1,79 @@
+package certs
+
+import (
+	"bufio"
+	"github.com/sirupsen/logrus"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+type KubernetesCertsGenerator interface {
+	//Generate generates a group of certificates that be uses for communicating with Kubernetes components.
+	Generate(path string) (*GeneratedCertsMap, error)
+}
+
+type KubernetesCertsGeneratorImple struct {
+}
+
+type GeneratedCertsMap struct {
+	res  map[string]string
+	path string
+}
+
+func (gm *GeneratedCertsMap) CleanResource() {
+	if _, err := os.Stat(gm.path); err != nil && os.IsExist(err) {
+		_ = os.RemoveAll(gm.path)
+	}
+}
+
+func (gm *GeneratedCertsMap) GetResources() map[string]string {
+	return gm.res
+}
+
+func (kci *KubernetesCertsGeneratorImple) Generate(path string) (*GeneratedCertsMap, error) {
+	cmd := exec.Command("/bin/bash", "-c", "kubeadm init phase certs")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+	if err = cmd.Start(); err != nil {
+		return nil, err
+	}
+	m := GeneratedCertsMap{res: make(map[string]string), path: path}
+	reader := bufio.NewReader(stdout)
+	for {
+		traceData, _, err := reader.ReadLine()
+		if err != nil {
+			if err != io.EOF {
+				return nil, err
+			}
+			break
+		}
+		logrus.Infof(string(traceData))
+	}
+	if err = cmd.Wait(); err != nil {
+		return nil, err
+	}
+	err = filepath.Walk(path, func(p string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		f, err := os.OpenFile(filepath.Join(path, p), os.O_RDONLY, 0644)
+		if err != nil {
+			return err
+		}
+		fileData, err := ioutil.ReadAll(f)
+		if err != nil {
+			return err
+		}
+		m.res[info.Name()] = string(fileData)
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &m, nil
+}
