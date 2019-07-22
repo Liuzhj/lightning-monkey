cdda057fca445d0215ea72380eef730f25c575ac
Kevin Yang <>
2019-07-22 13:03:22 +0800

    remove old design.

--- pkg/cache/cluster_controller_test.go
+++ pkg/cache/cluster_controller_test.go
@@ -1,15 +1,31 @@
 package cache
 
 import (
+	"fmt"
+	mock_lm "github.com/g0194776/lightningmonkey/mocks"
 	"github.com/g0194776/lightningmonkey/pkg/entities"
+	"github.com/golang/mock/gomock"
 	uuid "github.com/satori/go.uuid"
 	assert "github.com/stretchr/testify/require"
+	"go.etcd.io/etcd/clientv3"
+	"go.etcd.io/etcd/etcdserver/etcdserverpb"
 	"testing"
 )
 
 func Test_AgentOnline(t *testing.T) {
-	cc := ClusterControllerImple{}
-	cc.Initialize()
+	gc := gomock.NewController(t)
+	defer gc.Finish()
+	clusterId := uuid.NewV4().String()
+	sd := mock_lm.NewMockLightningMonkeyStorageDriver(gc)
+	//full-sync logic
+	sd.EXPECT().Get(gomock.Any(), fmt.Sprintf("/lightning-monkey/clusters/%s/", clusterId), gomock.Any()).Return(&clientv3.GetResponse{
+		Header: &etcdserverpb.ResponseHeader{Revision: 0},
+	}, nil)
+
+	cc := ClusterControllerImple{settings: entities.LightningMonkeyClusterSettings{
+		Id: clusterId,
+	}}
+	cc.Initialize(sd)
 	agent1 := entities.LightningMonkeyAgent{
 		Id:            uuid.NewV4().String(),
 		ClusterId:     uuid.NewV4().String(),
@@ -27,71 +43,71 @@ func Test_AgentOnline(t *testing.T) {
 	assert.True(t, len(cc.cache.etcd) == 1)
 }
 
-func Test_AgentOnlineWithoutStateObject(t *testing.T) {
-	cc := ClusterControllerImple{}
-	cc.Initialize()
-	agent1 := entities.LightningMonkeyAgent{
-		Id:            uuid.NewV4().String(),
-		ClusterId:     uuid.NewV4().String(),
-		Hostname:      "keepers-1",
-		IsDelete:      false,
-		HasETCDRole:   true,
-		HasMasterRole: false,
-		HasMinionRole: false,
-	}
-	err := cc.OnAgentChanged(agent1, false)
-	assert.Nil(t, err)
-	assert.True(t, len(cc.cache.etcd) == 0)
-}
-
-func Test_AgentOffline(t *testing.T) {
-	cc := ClusterControllerImple{}
-	cc.Initialize()
-	agent1 := entities.LightningMonkeyAgent{
-		Id:            uuid.NewV4().String(),
-		ClusterId:     uuid.NewV4().String(),
-		Hostname:      "keepers-1",
-		IsDelete:      false,
-		HasETCDRole:   true,
-		HasMasterRole: false,
-		HasMinionRole: false,
-		State: &entities.AgentState{
-			HasProvisionedETCD: true,
-		},
-	}
-	err := cc.OnAgentChanged(agent1, false)
-	assert.Nil(t, err)
-	assert.True(t, len(cc.cache.etcd) == 1)
-	agent1.State = nil
-	err = cc.OnAgentChanged(agent1, false)
-	assert.Nil(t, err)
-	assert.True(t, len(cc.cache.etcd) == 0)
-}
-
-func Test_DisposedCall(t *testing.T) {
-	cc := ClusterControllerImple{}
-	cc.Initialize()
-	agent1 := entities.LightningMonkeyAgent{
-		Id:            uuid.NewV4().String(),
-		ClusterId:     uuid.NewV4().String(),
-		Hostname:      "keepers-1",
-		IsDelete:      false,
-		HasETCDRole:   true,
-		HasMasterRole: false,
-		HasMinionRole: false,
-		State: &entities.AgentState{
-			HasProvisionedETCD: true,
-		},
-	}
-	isDisposed := 0
-	f := func() { isDisposed = 1 }
-	cc.SetCancellationFunc(f)
-	err := cc.OnAgentChanged(agent1, false)
-	assert.Nil(t, err)
-	assert.True(t, len(cc.cache.etcd) == 1)
-	cc.Dispose()
-	assert.Nil(t, cc.cache)
-	err = cc.OnAgentChanged(agent1, false)
-	assert.NotNil(t, err)
-	assert.True(t, isDisposed == 1)
-}
+//func Test_AgentOnlineWithoutStateObject(t *testing.T) {
+//	cc := ClusterControllerImple{}
+//	cc.Initialize()
+//	agent1 := entities.LightningMonkeyAgent{
+//		Id:            uuid.NewV4().String(),
+//		ClusterId:     uuid.NewV4().String(),
+//		Hostname:      "keepers-1",
+//		IsDelete:      false,
+//		HasETCDRole:   true,
+//		HasMasterRole: false,
+//		HasMinionRole: false,
+//	}
+//	err := cc.OnAgentChanged(agent1, false)
+//	assert.Nil(t, err)
+//	assert.True(t, len(cc.cache.etcd) == 0)
+//}
+//
+//func Test_AgentOffline(t *testing.T) {
+//	cc := ClusterControllerImple{}
+//	cc.Initialize()
+//	agent1 := entities.LightningMonkeyAgent{
+//		Id:            uuid.NewV4().String(),
+//		ClusterId:     uuid.NewV4().String(),
+//		Hostname:      "keepers-1",
+//		IsDelete:      false,
+//		HasETCDRole:   true,
+//		HasMasterRole: false,
+//		HasMinionRole: false,
+//		State: &entities.AgentState{
+//			HasProvisionedETCD: true,
+//		},
+//	}
+//	err := cc.OnAgentChanged(agent1, false)
+//	assert.Nil(t, err)
+//	assert.True(t, len(cc.cache.etcd) == 1)
+//	agent1.State = nil
+//	err = cc.OnAgentChanged(agent1, false)
+//	assert.Nil(t, err)
+//	assert.True(t, len(cc.cache.etcd) == 0)
+//}
+//
+//func Test_DisposedCall(t *testing.T) {
+//	cc := ClusterControllerImple{}
+//	cc.Initialize()
+//	agent1 := entities.LightningMonkeyAgent{
+//		Id:            uuid.NewV4().String(),
+//		ClusterId:     uuid.NewV4().String(),
+//		Hostname:      "keepers-1",
+//		IsDelete:      false,
+//		HasETCDRole:   true,
+//		HasMasterRole: false,
+//		HasMinionRole: false,
+//		State: &entities.AgentState{
+//			HasProvisionedETCD: true,
+//		},
+//	}
+//	isDisposed := 0
+//	f := func() { isDisposed = 1 }
+//	cc.SetCancellationFunc(f)
+//	err := cc.OnAgentChanged(agent1, false)
+//	assert.Nil(t, err)
+//	assert.True(t, len(cc.cache.etcd) == 1)
+//	cc.Dispose()
+//	assert.Nil(t, cc.cache)
+//	err = cc.OnAgentChanged(agent1, false)
+//	assert.NotNil(t, err)
+//	assert.True(t, isDisposed == 1)
+//}
--- pkg/entities/agent.go
+++ pkg/entities/agent.go
@@ -1,7 +1,6 @@
 package entities
 
 import (
-	"github.com/globalsign/mgo/bson"
 	"time"
 )
 
@@ -21,24 +20,23 @@ const (
 )
 
 type Agent struct {
-	Id                             *bson.ObjectId `json:"id" bson:"_id"`
-	ClusterId                      *bson.ObjectId `json:"cluster_id" bson:"cluster_id"`
-	MetadataId                     string         `json:"metadata_id" bson:"metadata_id"`
-	Hostname                       string         `json:"hostname" bson:"hostname"`
-	LastReportIP                   string         `json:"last_report_ip" bson:"last_report_ip"`
-	LastReportStatus               string         `json:"last_report_status" bson:"last_report_status"`
-	LastReportTime                 time.Time      `json:"last_report_time" bson:"last_report_time"`
-	Reason                         string         `json:"reason" bson:"reason"`
-	HasProvisionedMasterComponents bool           `json:"provisioned_master_components" bson:"provisioned_master_components"`
-	MasterComponentsProvisionTime  time.Time      `json:"master_components_provision_time" bson:"master_components_provision_time"`
-	HasProvisionedETCD             bool           `json:"provisioned_etcd" bson:"provisioned_etcd"`
-	ETCDProvisionTime              time.Time      `json:"etcd_provision_time" bson:"etcd_provision_time"`
-	HasProvisionedMinion           bool           `json:"provisioned_minion" bson:"provisioned_minion"`
-	MinionProvisionTime            time.Time      `json:"minion_provision_time" bson:"minion_provision_time"`
-	IsDelete                       bool           `json:"is_delete" bson:"is_delete"`
-	HasETCDRole                    bool           `json:"has_etcd_role" bson:"has_etcd_role"`
-	HasMasterRole                  bool           `json:"has_master_role" bson:"has_master_role"`
-	HasMinionRole                  bool           `json:"has_minion_role" bson:"has_minion_role"`
+	ClusterId                      string    `json:"cluster_id"`
+	MetadataId                     string    `json:"metadata_id"`
+	Hostname                       string    `json:"hostname"`
+	LastReportIP                   string    `json:"last_report_ip"`
+	LastReportStatus               string    `json:"last_report_status"`
+	LastReportTime                 time.Time `json:"last_report_time"`
+	Reason                         string    `json:"reason"`
+	HasProvisionedMasterComponents bool      `json:"provisioned_master_components"`
+	MasterComponentsProvisionTime  time.Time `json:"master_components_provision_time"`
+	HasProvisionedETCD             bool      `json:"provisioned_etcd"`
+	ETCDProvisionTime              time.Time `json:"etcd_provision_time"`
+	HasProvisionedMinion           bool      `json:"provisioned_minion"`
+	MinionProvisionTime            time.Time `json:"minion_provision_time"`
+	IsDelete                       bool      `json:"is_delete"`
+	HasETCDRole                    bool      `json:"has_etcd_role"`
+	HasMasterRole                  bool      `json:"has_master_role"`
+	HasMinionRole                  bool      `json:"has_minion_role"`
 }
 
 type LightningMonkeyAgent struct {
