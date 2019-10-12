fbc49d91fcf2ed1c3c7edc5a746b37265bf20d8c
Kevin Yang <yangjie1@gridsum.com>
2019-10-12 14:41:25 +0800

    add deployment support for ElasticSearch cluster.

---
+++ pkg/controllers/elasticsearch/vars.go
@@ -0,0 +1,9 @@
+package elasticsearch
+
+const (
+	VAR_MASTER_XMS   = "master_xms"
+	VAR_MASTER_XMX   = "master_xmx"
+	VAR_DATA_XMS     = "data_xms"
+	VAR_DATA_XMX     = "data_xmx"
+	VAR_PING_TIMEOUT = "ping_timeout"
+)
--- pkg/entities/cluster.go
+++ pkg/entities/cluster.go
@@ -43,6 +43,7 @@ const (
 	EXT_DEPLOYMENT_ALTERMANAGER                          = "altermanager"
 	EXT_DEPLOYMENT_METRICSERVER                          = "metric-server"
 	EXT_DEPLOYMENT_TRAEFIK                               = "traefik"
+	EXT_DEPLOYMENT_ES                                    = "es"
 	_                                    AgentStatusFlag = iota
 	AgentStatusFlag_Whatever
 	AgentStatusFlag_Running
