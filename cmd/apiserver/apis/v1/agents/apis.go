60189762de227869a78d4c66a63c9e0275fe629c
Kevin Yang <>
2019-12-23 17:47:52 +0800

    support LAZY execution for changing specific agent's cluster.

--- cmd/apiserver/apis/v1/agents/apis.go
+++ cmd/apiserver/apis/v1/agents/apis.go
@@ -1,10 +1,13 @@
 package agents
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"strconv"
+	"sync"
+	"time"
 
 	"github.com/g0194776/lightningmonkey/pkg/common"
 	"github.com/g0194776/lightningmonkey/pkg/entities"
@@ -13,12 +16,53 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+type PendingTask struct {
+	isCancelled bool
+	cancel      func()
+	workProc    func()
+	ct          context.Context
+	agentId     string
+}
+
+func (t *PendingTask) Cancel() {
+	t.isCancelled = true
+	t.cancel()
+}
+
+func (t *PendingTask) DoAsync() {
+	go func() {
+		select {
+		case <-t.ct.Done():
+			//remove itself from the background task list.
+			pendingTaskLock.Lock()
+			delete(pendingTasks, t.agentId)
+			pendingTaskLock.Unlock()
+			if t.isCancelled {
+				return
+			}
+		}
+		t.workProc()
+	}()
+}
+
+var (
+	pendingTaskLock *sync.RWMutex
+	pendingTasks    map[string] /*agent id*/ *PendingTask
+)
+
 func Register(app *iris.Application) error {
+	if pendingTasks == nil {
+		pendingTasks = make(map[string] /*agent id*/ *PendingTask)
+	}
+	if pendingTaskLock == nil {
+		pendingTaskLock = &sync.RWMutex{}
+	}
 	logrus.Infof("    Registering Agents Mgmt APIs...")
 	app.Post("/apis/v1/agent/register", RegisterAgent)
 	app.Get("/apis/v1/agent/query", AgentQueryNextWork)
 	app.Put("/apis/v1/agent/status", ReportStatus)
 	app.Put("/apis/v1/agent/change", ChangeAgentClusterAndRoles)
+	app.Delete("/apis/v1/agent/change", CancelChangeAgentClusterAndRoles)
 	app.Get("/apis/v1/agents/list", ListAgentsByClusterId)
 	return nil
 }
@@ -230,6 +274,20 @@ func ChangeAgentClusterAndRoles(ctx iris.Context) {
 	isMasterRole := ctx.URLParamInt32Default("master", 0) == 1
 	isMinionRole := ctx.URLParamInt32Default("minion", 0) == 1
 	isHARole := ctx.URLParamInt32Default("ha", 0) == 1
+	waitTimeSecs := ctx.URLParamInt32Default("wait", 0)
+	if waitTimeSecs > 0 {
+		var innerRsp entities.Response
+		err := addPendingTask(agentId, waitTimeSecs, newClusterId, oldClusterId, agent, isETCDRole, isMasterRole, isMinionRole, isHARole)
+		if err != nil {
+			innerRsp = entities.Response{ErrorId: entities.OperationFailed, Reason: err.Error()}
+		} else {
+			innerRsp = entities.Response{ErrorId: entities.Succeed, Reason: fmt.Sprintf("Waiting, After %d seconds the agent %s will be change to cluster %s!", waitTimeSecs, agentId, newClusterId)}
+		}
+		ctx.JSON(&innerRsp)
+		ctx.Values().Set(entities.RESPONSEINFO, &innerRsp)
+		ctx.Next()
+		return
+	}
 	err = common.ClusterManager.TransferAgentToCluster(
 		oldClusterId,
 		newClusterId,
@@ -249,6 +307,66 @@ func ChangeAgentClusterAndRoles(ctx iris.Context) {
 	ctx.JSON(&rsp)
 	ctx.Values().Set(entities.RESPONSEINFO, &rsp)
 	ctx.Next()
+}
+
+func CancelChangeAgentClusterAndRoles(ctx iris.Context) {
+	agentId := ctx.URLParam("agent-id")
+	if agentId == "" {
+		rsp := entities.Response{ErrorId: entities.ParameterError, Reason: "\"agent-id\" parameter is required."}
+		ctx.JSON(&rsp)
+		ctx.Values().Set(entities.RESPONSEINFO, &rsp)
+		ctx.Next()
+		return
+	}
+	pendingTaskLock.Lock()
+	defer pendingTaskLock.Unlock()
+	var isOK bool
+	var t *PendingTask
+	if t, isOK = pendingTasks[agentId]; isOK {
+		delete(pendingTasks, agentId)
+		t.Cancel()
+	}
+	rsp := entities.Response{ErrorId: entities.Succeed}
+	ctx.JSON(&rsp)
+	ctx.Values().Set(entities.RESPONSEINFO, &rsp)
+	ctx.Next()
+}
+
+func addPendingTask(agentId string, waitTimeSecs int32, newClusterId, oldClusterId string, agent *entities.LightningMonkeyAgent, isETCDRole, isMasterRole, isMinionRole, isHARole bool) error {
+	pendingTaskLock.Lock()
+	defer pendingTaskLock.Unlock()
+	var isOK bool
+	var t *PendingTask
+	if t, isOK = pendingTasks[agentId]; isOK {
+		return fmt.Errorf("Duplicated background task for agent %s!", agentId)
+	}
+	t = NewPendingTask(agentId, int(waitTimeSecs), func() {
+		logrus.Warnf("Delay triggered by background task, changing agent %s to cluster %s...", agentId, newClusterId)
+		err := common.ClusterManager.TransferAgentToCluster(
+			oldClusterId,
+			newClusterId,
+			agent,
+			isETCDRole,
+			isMasterRole,
+			isMinionRole,
+			isHARole)
+		if err != nil {
+			if agent.State != nil {
+				msg := fmt.Sprintf("Failed to change agent %s to cluster %s, error: %s", agentId, newClusterId, err.Error())
+				logrus.Error(msg)
+				agent.State.Reason = msg
+			}
+		}
+	})
+	pendingTasks[agentId] = t
+	t.DoAsync()
+	return nil
+}
+
+func NewPendingTask(agentId string, waitTime int, workProc func()) *PendingTask {
+	d, _ := time.ParseDuration(fmt.Sprintf("%ds", waitTime))
+	ct, cf := context.WithTimeout(context.Background(), d)
+	return &PendingTask{agentId: agentId, ct: ct, cancel: cf, workProc: workProc}
 }
 
 func ListAgentsByClusterId(ctx iris.Context) {
