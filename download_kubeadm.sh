0836c19aec24c82fcd7dfa92cf3bb7aeda30e9a4
Kevin Yang <yangjie1@gridsum.com>
2020-02-11 11:43:43 +0800

    add CNI installation.

--- download_kubeadm.sh
+++ download_kubeadm.sh
@@ -5,6 +5,6 @@ arr=$1
 for ver in ${arr[*]} ; do
     mkdir ${ver}
     #wget -v https://dl.k8s.io/v${ver}/kubernetes-node-linux-amd64.tar.gz -o ./${ver}/kubernetes-node-linux-amd64.tar.gz
-    wget -v https://storage.googleapis.com/kubernetes-release/release/v${ver}/kubernetes-node-linux-amd64.tar.gz -o ./${ver}/kubernetes-node-linux-amd64.tar.gz
-    tar -zxvf ./${ver}/kubernetes-node-linux-amd64.tar.gz
+    wget -v -O ./${ver}/kubernetes-node-linux-amd64.tar.gz https://storage.googleapis.com/kubernetes-release/release/v${ver}/kubernetes-node-linux-amd64.tar.gz
+    tar -zxvf ./${ver}/kubernetes-node-linux-amd64.tar.gz -C ./${ver}
 done
